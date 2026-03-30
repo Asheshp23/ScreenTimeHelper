2589899b28afd11dcb829c2fee3d49d7fc1d3165
asheshp23 <asheshp23@gmail.com>
2026-03-30 09:13:21 -0400

    Initial Commit

---
+++ ScreenTimeHelper/ScreenTimeHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeHelperApp.swift
+//  ScreenTimeHelper
+//
+//  Created by Ashesh Patel on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
